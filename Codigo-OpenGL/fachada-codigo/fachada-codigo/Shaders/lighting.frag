6144c604bc0e6b0cac4a7db0f7d02864ff5c2cd4
cgiovvnni <cgiovanni@comunidad.unam.mx>
2021-06-19 17:33:21 -0500

    Creamos proyecto en OpengGL

---
+++ Codigo-OpenGL/fachada-codigo/fachada-codigo/Shaders/lighting.frag
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e7a74533a3e6b0e0f345a439ba868086c4dd7e2d79690fa6d6a1edaccdddaa6
+size 5602
---
+++ Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:adaaeae5c680854c46f387365c53c1a51a6c2f41f1cd8ab767fa098c0842e74f
+size 27283
