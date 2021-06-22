ef0cf11e1603a46e1ca04c8108c1c37c3997029b
cgiovvnni <cgiovanni@comunidad.unam.mx>
2021-06-22 15:43:14 -0500

    Feat:Carga de Skybox en OpenGL

--- Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
+++ Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adaaeae5c680854c46f387365c53c1a51a6c2f41f1cd8ab767fa098c0842e74f
-size 27283
+oid sha256:7b4933d54bf2c8a63c1a7147286410e721d87879a1e18d269c20889011a888c7
+size 27337
