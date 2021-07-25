613012e37ec6d54d86ea61363c4e119e0503a880
cgiovvnni <cgiovanni@comunidad.unam.mx>
2021-07-25 03:20:34 -0500

    Feat: Animaciones simples y con KeyFrames en OpenGL

--- Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
+++ Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9edb4ec4d4475f9209d1edcbd056e095226507e8abe7e493ab6503ae488cb50f
-size 31490
+oid sha256:5fdbc5d5df89e410680f0c3d16df052f8eb729ffc1ecd066672afcfa8a5afa1f
+size 38197
