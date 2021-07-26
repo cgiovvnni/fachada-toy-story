7dabf7d76c82c7d648eb8d712c2887704a6eeaf4
cgiovvnni <cgiovanni@comunidad.unam.mx>
2021-07-25 23:59:54 -0500

    Feat: limitaciones en animaciones simples

--- Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
+++ Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fdbc5d5df89e410680f0c3d16df052f8eb729ffc1ecd066672afcfa8a5afa1f
-size 38197
+oid sha256:d1e19efe25c6083217897e3e2c2b1c77e8f0c5fbe847c58b6a628021a3dd18a9
+size 34920
