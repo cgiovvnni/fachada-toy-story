bfb7650cb76c133f0c7dc1b8250917aaccfb3ea8
cgiovvnni <cgiovanni@comunidad.unam.mx>
2021-08-12 18:03:32 -0500

    Fix: Correccion recorrido de personaje

--- Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
+++ Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8bd8064ddbd5149a775cf51ad981fec397f506eb42682aedbc6eace42dbf8e6
-size 40446
+oid sha256:81e199b4c53ba57f6749dd34f2f7dc90dbc67b019806cf18e3c5b5bcca330cdb
+size 44445
