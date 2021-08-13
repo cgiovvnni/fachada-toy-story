2b82cf23a3226b1808d2efaf3c7dd0bdd9dcd3c4
cgiovvnni <cgiovanni@comunidad.unam.mx>
2021-08-12 22:25:32 -0500

    Fix: correccion dimension de ventana y velocidad keyframes

--- Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
+++ Codigo-OpenGL/fachada-codigo/fachada-codigo/fachada.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81e199b4c53ba57f6749dd34f2f7dc90dbc67b019806cf18e3c5b5bcca330cdb
-size 44445
+oid sha256:22a2bbbff4e3367f9165b2b997b274a8f9a6f1e633af776691f3a3c2c9b9659b
+size 44403
